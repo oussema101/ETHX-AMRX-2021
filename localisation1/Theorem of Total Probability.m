ae1e843224c6b2e981f7f3541805bd6c84fbef5d
Oussema Jouini <oussemajouini@ieee.org>
2021-08-11 18:56:00 +0100

    Add files via upload

---
+++ localisation1/Theorem of Total Probability.m
@@ -0,0 +1,14 @@
+% Probability of heads.
+PHeads = 0.5;
+        
+% Probability of tails.
+PTails = 0.5;
+        
+% Probability of a black ball given heads.
+PBlackGHeads = 0.3;
+        
+% Probability of a black ball given tails.
+PBlackGTails = 0.5;
+        
+% Total probability of picking a black ball.
+PBlack = 0.4;
